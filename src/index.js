ac6f872681ce0ab1e4af11b20653b038b1efbea5
Natalya Zernova <nz_18@mail.ru>
2021-04-22 23:33:01 +0300

    add the task on cycles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const numberOfFilms =  Number(prompt('Сколько фильмов вы  уже посмотрели?', ''));
 
 const  personalMovieDB = {
@@ -9,12 +11,33 @@ const  personalMovieDB = {
 };
 
 let a = prompt('Один из последних просмотренных фильмов?', '');
+
+while ( a === null || a.length === 0 || a.length > 50 ) {
+  a = prompt('Один из последних просмотренных фильмов?', '');
+};
+
+// let c = prompt('Один из последних просмотренных фильмов?', '');
+// let d = Number(prompt('На сколько оцените его?', ''));
+
 let b = Number(prompt('На сколько оцените его?', ''));
-let c = prompt('Один из последних просмотренных фильмов?', '');
-let d = Number(prompt('На сколько оцените его?', ''));
+console.log(b)
+while ( b === 0 || b.length === 1 ) {
+  b = Number(prompt('На сколько оцените его?', ''));
+};
+console.log(b);
+
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert('Вы классический зритель!');
+} else if (personalMovieDB.count > 30) {
+  alert('Вы киноман')
+} else {
+  alert('Произошла ошибка')
+};
